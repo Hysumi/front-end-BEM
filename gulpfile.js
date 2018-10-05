910596eb1d1d1accac2cae1ac540326c807b8f1e
hysumi <henrique.sumitomo@gmail.com>
2018-10-05 04:50:36 -0300

    configurar gulp e inicio do header

--- gulpfile.js
+++ gulpfile.js
@@ -12,37 +12,19 @@ var changed = require("gulp-changed");
 /*
  * Path
  */
-var SCSS_SRC_DESKTOP = "./src/desktop.bundles/bundle/*.scss";
-var SCSS_SRC_MOBILE = "./src/mobile.bundles/bundle/*.scss";
-
-var SCSS_DEST_DESKTOP = "./src/desktop.bundles/bundle/";
-var SCSS_DEST_MOBILE = "./src/mobile.bundles/bundle/";
+var SCSS_SRC = "./src/bundles/css-bundle/*.scss";
+var SCSS_DEST = "./src/bundles/css-bundle/";
 
 /*
  * Compile SCSS
  */
 
-gulp.task("compile_mobile_scss", function() {
-    return gulp.src(SCSS_SRC_MOBILE)
-    .pipe(sass().on("error", sass.logError))
-    .pipe(minifyCSS())
-    .pipe(rename({suffix: ".min"}))
-    .pipe(changed(SCSS_DEST_MOBILE))
-    .pipe(gulp.dest(SCSS_DEST_MOBILE));
-});
-
-gulp.task("compile_desktop_scss", function() {
-    return gulp.src(SCSS_SRC_DESKTOP)
-    .pipe(sass().on("error", sass.logError))
-    .pipe(minifyCSS())
-    .pipe(rename({suffix: ".min"}))
-    .pipe(changed(SCSS_DEST_DESKTOP))
-    .pipe(gulp.dest(SCSS_DEST_DESKTOP));
-});
-
 gulp.task("compile_scss", function () {
-    gulp.start("compile_mobile_scss");
-    gulp.start("compile_desktop_scss");
+    return gulp.src(SCSS_SRC)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(minifyCSS())
+    .pipe(rename({suffix: ".min"}))
+    .pipe(gulp.dest(SCSS_DEST));
 });
 
 /*
@@ -50,7 +32,7 @@ gulp.task("compile_scss", function () {
  */
 
 gulp.task("watch_scss", function () {
-    gulp.watch([SCSS_SRC_DESKTOP, SCSS_SRC_MOBILE], ["compile_scss"]);
+    gulp.watch([SCSS_SRC], ["compile_scss"]);
 });
 
 /*
